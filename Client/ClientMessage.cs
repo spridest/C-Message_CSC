b15c83a688a8550ceab47bc326ce3aef1c11306d
spridest <spridest@yahoo.com.tw>
2023-02-28 22:09:07 +0800

    init

---
+++ Client/ClientMessage.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+
+public class Client
+{
+    private static readonly string serverIP = "127.0.0.1"; // 伺服器 IP
+    private static readonly int serverPort = 8888; // 伺服器 Port
+
+    private static TcpClient client = null;
+    private static StreamWriter writer = null;
+
+    public static void Main()
+    {
+        try
+        {
+            client = new TcpClient(serverIP, serverPort);
+            Console.WriteLine("Connected to server!");
+
+            // 取得網路串流
+            NetworkStream stream = client.GetStream();
+
+            // 設定編碼方式
+            StreamReader reader = new StreamReader(stream);
+            writer = new StreamWriter(stream);
+            writer.AutoFlush = true;
+
+            // 開啟一個執行緒處理伺服器回傳的訊息
+            Thread thread = new Thread(ReceiveMessage);
+            thread.Start();
+
+            // 等待使用者輸入訊息
+            string message = "";
+            while (message != "exit")
+            {
+                Console.WriteLine("Message: ");
+                message = Console.ReadLine();
+                SendMessage(message);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            // 關閉連線
+            client.Close();
+        }
+    }
+
+    // 傳送訊息給伺服器
+    private static void SendMessage(string message)
+    {
+        writer.WriteLine(message);
+    }
+
+    // 接收伺服器回傳的訊息
+    private static void ReceiveMessage()
+    {
+        try
+        {
+            // 取得網路串流
+            NetworkStream stream = client.GetStream();
+
+            // 設定編碼方式
+            StreamReader reader = new StreamReader(stream);
+
+            while (true)
+            {
+                // 讀取伺服器回傳的訊息
+                string message = reader.ReadLine();
+
+                if (message == null)
+                {
+                    Console.WriteLine("Disconnected from server.");
+                    break;
+                }
+
+                // 顯示伺服器回傳的訊息
+                Console.WriteLine("Server: " + message);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
---
+++ Server/SeverMessage.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Server
+{
+    class Program
+    {
+        // 儲存所有客戶端連線
+        static List<TcpClient> clients = new List<TcpClient>();
+
+        static void Main(string[] args)
+        {
+            // 伺服器 IP 與 Port
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            int port = 8888;
+
+            // 建立監聽 Socket
+            TcpListener listener = new TcpListener(ip, port);
+            listener.Start();
+            Console.WriteLine("伺服器已啟動");
+
+            // 接受客戶端連線，並建立新執行緒處理連線
+            while (true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                Console.WriteLine("有新客戶端連線");
+
+                Thread t = new Thread(new ParameterizedThreadStart(HandleClient));
+                t.Start(client);
+            }
+        }
+
+        // 處理客戶端連線
+        static void HandleClient(object obj)
+        {
+            TcpClient client = (TcpClient)obj;
+            clients.Add(client);
+
+            // 取得用戶端資訊
+            string clientInfo = client.Client.RemoteEndPoint.ToString();
+            Console.WriteLine("客戶端 " + clientInfo + " 已連線");
+
+            // 設定編碼方式
+            Encoding encoding = Encoding.UTF8;
+
+            // 處理客戶端發送的訊息
+            while (true)
+            {
+                // 讀取客戶端發送的訊息
+                byte[] buffer = new byte[1024];
+                int n = client.GetStream().Read(buffer, 0, buffer.Length);
+                string message = encoding.GetString(buffer, 0, n);
+                Console.WriteLine("客戶端 " + clientInfo + " 傳送了訊息：" + message);
+
+                // 轉發訊息給其他客戶端
+                foreach (TcpClient c in clients)
+                {
+                    if (c != client)
+                    {
+                        c.GetStream().Write(buffer, 0, n);
+                    }
+                }
+
+                if (message == "exit")
+                {
+                    // 關閉連線
+                    // client.Close();
+                    int index = clients.FindIndex(v => v.Client.RemoteEndPoint.ToString() == clientInfo);
+                    clients.RemoveAt(index);
+                    Console.WriteLine("客戶端 " + clientInfo + " 已斷線");
+                }
+            }
+
+            
+            
+        }
+
+        
+    }
+}
